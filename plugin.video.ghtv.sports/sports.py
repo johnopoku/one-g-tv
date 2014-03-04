37c0ab4e4b3794b3f22ecae86a8b684f1b40458e
owusu-banahene <stephenoyeboah@gmail.com>
2014-03-04 09:41:35 -0500

    Sports plugin that automatically play

---
+++ plugin.video.ghtv.sports/sports.py
@@ -0,0 +1,21 @@
+import sys
+import xbmc
+import xbmcgui
+import xbmcplugin
+import xbmcaddon
+
+addon_handler = int(sys.argv[1])
+addon = xbmcaddon.Addon('plugin.video.ghtv.sports')
+title = addon.getAddonInfo('name')
+icon = addon.getAddonInfo('icon')
+xbmcplugin.setContent(addon_handler, 'video')
+playlist = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
+playlist.clear()
+playlist.add('http://erp.dubtel.tv/XBMC/chel-man.mp4')
+playlist.add('http://erp.dubtel.tv/XBMC/gha-ept.mp4')
+li = xbmcgui.ListItem(label=title, iconImage=icon, thumbnailImage=icon)
+li.setInfo(type='Video', infoLabels={"Title": title})
+li.setProperty('IsPlayable', 'true')
+xbmcplugin.setResolvedUrl(addon_handler, True, li)
+xbmc.Player(xbmc.PLAYER_CORE_MPLAYER).play(item=playlist, listitem=li)
+
